97aea00f11d125a6d854d01778ef65efb4b1cfdc
emptysoal <57931586+emptysoal@users.noreply.github.com>
2023-10-28 10:44:15 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -91,7 +91,7 @@ int run(char* videoPath)
                 cv::Point(0, 30), 0, 0.6, cv::Scalar(0, 0, 255), 2, cv::LINE_AA);
         writer.write(img);
 
-        cv::imshow("img", img);
+        // cv::imshow("img", img);
         int c = cv::waitKey(1);
 	if (c == 27) break;  // ESC to exit
     }
